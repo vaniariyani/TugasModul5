f4137a5fda879ec1484095721a3e25b555afe3c6
Vania Ariyani Prilia Putri <kazusaakirioo@gmail.com>
2018-11-09 11:18:50 +0700

    First commit

---
+++ service-worker.js
@@ -0,0 +1,81 @@
+var dataCacheName = 'weatherData-v1';
+var cacheName = 'weatherPWA-step-6-1';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/logo/icon/LTV7_Logo.png',
+  '/scripts/app.js',
+  '/styles/inline.css',
+  '/logo/clear.png',
+  '/logo/cloudy-scattered-showers.png',
+  '/logo/cloudy.png',
+  '/logo/fog.png',
+  '/images/ic_add_white_24px.svg',
+  '/images/ic_refresh_white_24px.svg',
+  '/logo/partly-cloudy.png',
+  '/logo/rain.png',
+  '/logo/scattered-showers.png',
+  '/logo/sleet.png',
+  '/logo/snow.png',
+  '/logo/thunderstorm.png',
+  '/logo/wind.png'
+];
+
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+
+self.addEventListener('fetch', function(e) {
+  console.log('[Service Worker] Fetch', e.request.url);
+  var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+  if (e.request.url.indexOf(dataUrl) > -1) {
+    /*
+     * When the request URL contains dataUrl, the app is asking for fresh
+     * weather data. In this case, the service worker always goes to the
+     * network and then caches the response. This is called the "Cache then
+     * network" strategy:
+     */
+    e.respondWith(
+      caches.open(dataCacheName).then(function(cache) {
+        return fetch(e.request).then(function(response){
+          cache.put(e.request.url, response.clone());
+          return response;
+        });
+      })
+    );
+  } else {
+    /*
+     * The app is asking for app shell files. In this scenario the app uses the
+     * "Cache, falling back to the network" offline strategy:
+     */
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  }
+});
+
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
+
